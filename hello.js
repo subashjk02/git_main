b89a45c4890fc477feb462aa33346dcb1519533f
Subash <subashr654@gmail.com>
2025-06-08 17:50:02 +0530

    Revert "add new line hello.js file"
    This reverts commit 534dce7f182749097b8474de6e5682a7e1ab9f54.

--- hello.js
+++ hello.js
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 console.log("Hello, World!");
 console.log("This is a simple Node.js script.");
 console.log("TRYING OUT REVERT");
+=======
+console.log("Hello, World!");
+>>>>>>> parent of 534dce7 (add new line hello.js file)
