cd61bfd1005908243170bd4d9f139fe3e3308968
Subash <subashr654@gmail.com>
2025-06-08 23:30:12 +0530

    implementing urgent fix

--- hello.js
+++ hello.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 console.log("Hello, World!");
 console.log("This is a simple Node.js script.");
 console.log("TRYING OUT REVERT");
-=======
-console.log("Hello, World!");
->>>>>>> parent of 534dce7 (add new line hello.js file)
+//IMPLEMENTING URGINT FIX
